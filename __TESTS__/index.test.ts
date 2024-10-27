47c6a874ed2bfdf1dd3452746a257c6fd9525699
Nicola Dal Pont <ndalpont@protonmail.com>
2024-10-27 13:36:44 +1030

    Remove deno for testing. Using public APIs

--- __TESTS__/index.test.ts
+++ __TESTS__/index.test.ts
@@ -2,61 +2,47 @@ import HTTPError from '../src/HTTPError.ts';
 import { HTTPRequestFactory } from '../src/index.ts';
 import { describe, expect, it } from 'vitest';
 
-const sessionModel = {
-  accessToken: 'the-access-token',
-};
 
 const factory = new HTTPRequestFactory().withLogLevel('debug');
-
 
 factory
   .withAPIConfig(
     {
       name: 'simple-api',
-      baseURL: 'http://localhost:8080/api',
+      baseURL: 'https://httpbin.org',
       responseBodyTransformer: (body, request) => {
-        const url = new URL(request.getConfig().url);
-        console.warn(url.pathname);
-        if (url.pathname === '/api/products') {
-          body.data = Object.keys(body.data).map((key) => ({
+        if (request.meta?.api?.endpointName === 'get-products') {
+          body.json.data = Object.keys(body.json.data).map((key) => ({
             id: key,
             ...body.data[key],
           }));
         }
-        return body;
+        return body.json;
+      },
+      headers: {
+        accepts: 'application/json',
       },
       endpoints: {
+        bearer: {
+          target: '/bearer',
+        },
         'get-product-by-id': {
-          target: '/product/{{productId}}',
-          method: 'GET',
+          method: 'POST',
+          target: '/anything',
         },
         'get-products': {
-          target: '/products',
-          method: 'GET',
-        },
-      },
-    },
-    {
-      name: 'admin-api',
-      baseURL: 'http://localhost:8080/admin',
-      meta: {
-        api_key: 'the-api-key',
-      },
-      endpoints: {
-        'get-product-full-data-by-id': {
-          target: '/product/{{productId}}',
-          method: 'GET',
+          target: '/anything',
+          method: 'POST',
         },
       },
     }
   )
   .withLogLevel('error')
   .when((request) => {
-    return request.meta?.api?.name === 'admin-api';
+    return request.meta?.api?.endpointName === 'bearer';
   })
   .withHeaders({
-    'X-API-KEY': (request) => request.meta!.api_key,
-    Authorization: () => `Bearer ${sessionModel.accessToken}`,
+    Authorization: () => `Bearer the-access-token`,
   });
 
 describe('HTTP Tests', () => {
@@ -73,6 +59,17 @@ describe('HTTP Tests', () => {
     it('test_transformed_response_body', async () => {
       const response = await factory
         .createAPIRequest('simple-api', 'get-products')
+        .withJSONBody({
+          status: 'ok',
+          data: {
+            '123': {
+              name: 'Product 123',
+            },
+            '456': {
+              name: 'Product 456',
+            },
+          },
+        })
         .execute();
       expect(Array.isArray(response.data)).toBeTruthy();
       expect(response.data).toHaveLength(2);
@@ -100,6 +97,13 @@ describe('HTTP Tests', () => {
       const result = await factory
         .createAPIRequest('simple-api', 'get-product-by-id')
         .withURLParam('productId', '123')
+        .withJSONBody({
+          status: 'ok',
+          data: {
+            id: '123',
+            name: 'Product 123',
+          },
+        })
         .execute();
 
       expect(result.status).toEqual('ok');
@@ -109,38 +113,23 @@ describe('HTTP Tests', () => {
       });
     });
     it('test_conditional_auth_headers_happy_path', async () => {
-      expect(async () => {
-        const result = await factory
-          .createAPIRequest('admin-api', 'get-product-full-data-by-id')
-          .withURLParam('productId', '123')
-          .execute();
-        expect(result?.status).toEqual('ok');
-        expect(result?.data).toEqual({
-          id: '123',
-          secret: 'secret',
-          name: 'Product 123',
-        });
-        return result;
-      }).not.toThrowError();
+      await expect(
+        factory.createAPIRequest('simple-api', 'bearer').execute()
+      ).resolves.not.toThrowError();
     });
     it('test_conditional_auth_headers_failure', async () => {
       try {
         const result = await factory
-          .createAPIRequest('admin-api', 'get-product-full-data-by-id')
-          .blank()
+          .createAPIRequest('simple-api', 'bearer')
+          .blank() //get rid of auto-headers
           .withURLParam('productId', '123')
           .execute();
-        expect(result?.status).toEqual('ok');
-        expect(result?.data).toEqual({
-          id: '123',
-          secret: 'secret',
-          name: 'Product 123',
-        });
+        
       } catch (e) {
         expect(e).toBeInstanceOf(HTTPError);
         expect(e).toHaveProperty('code', 401);
         expect(e.isUnauthorized()).toBeTruthy();
-        expect(e.body).toEqual('Missing required header Authorization');
+        expect(e.message).toEqual('UNAUTHORIZED');
       }
     });
 
@@ -162,6 +151,14 @@ describe('HTTP Tests', () => {
       const result = await factory
         .createAPIRequest('simple-api', 'get-product-by-id')
         .withURLParam('productId', '123')
+        .withJSONBody({
+          status: 'ok',
+          data: {
+            id: '123',
+            secret: 'secret',
+            name: 'Product 123',
+          },
+        })
         .withResponseInterceptor(async (fetchResponse) => {})
         .execute();
       expect(result).toHaveProperty('status', 'ok');
